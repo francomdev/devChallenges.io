4c0f3c3a12694c5c026c9575790efa92c45ace63
Franco Mayo <francomayodev@gmail.com>
2023-11-20 20:45:11 -0300

    finish page

--- coffee-listing/src/App.jsx
+++ coffee-listing/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
   const [listing, setListing] = useState([])
+  const [availableSelected, setAvailableSelected] = useState(false)
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json").then(
       res => res.json()
@@ -13,20 +14,35 @@ function App() {
     )}
   , [])
 
+  const handleClick = (button) => {
+      if (button === "all") {
+        setAvailableSelected(false)
+      } else if (button === "available") {
+        setAvailableSelected(true)
+      }
+  }
   
   return (
   <main>
+    <header>
+    <img className='cafe-bg' src="bg-cafe.jpg" alt="" />
+    </header>
     <section>
+    <img className='vector' src="vector.svg" alt="" />
     <h1>Our Collection</h1>
     <p>Introducing our Coffee Collection, 
       a selection of unique coffees from different roast types and origins, 
       expertly roasted in small batches and shipped fresh weekly.</p>
       <div className='buttons'>
-        <button>All Products</button> 
-        <button>Available now</button>
+        <button style={availableSelected? {background:'#1B1D1F'} : {background:'#6F757C'}}
+        onClick={() => handleClick('all')}>All Products</button> 
+        <button style={availableSelected? {background:'#6F757C'} : {background:'#1B1D1F'}}
+        onClick={() => handleClick('available')}>Available now</button>
       </div>
     <div className='listing-container'>
-    {listing.map( item => (
+    {
+    availableSelected? (
+    listing.filter(item => item.available === true).map( item => (
       <Card 
       key={item.id}
       name={item.name}
@@ -34,8 +50,24 @@ function App() {
       price={item.price}
       rating={item.rating}
       popular={item.popular}
+      votes={item.votes}
+      available={item.available}
       />
-    ))}
+    ))) : (
+      listing.map( item => (
+        <Card 
+        key={item.id}
+        name={item.name}
+        image={item.image}
+        price={item.price}
+        rating={item.rating}
+        popular={item.popular}
+        votes={item.votes}
+        available={item.available}
+        />
+      ))
+    )
+  }
     </div>
     </section>
   </main>
--- coffee-listing/src/components/Card.jsx
+++ coffee-listing/src/components/Card.jsx
@@ -2,13 +2,24 @@ import '../App.css'
 export default function Card(props) {
 return(
 <div className="card-container">
-    <img src={props.image} alt={props.name}/>
-    
+    <img className='coffee-img' src={props.image} alt={props.name}/>
+    {props.popular && <div className='popular-tag'>Popular</div>}
     <div className='card-name'>
         <h3>{props.name}</h3>
-        <h3>{props.price}</h3>
+        <h3 className='price'>{props.price}</h3>
     </div>
-    <h3>{props.rating}</h3>
+    {props.rating?
+    <div className='rating'>
+       <img src="/public/Star_fill.svg" alt="star" /> <h3 className='rating'>{props.rating}</h3>
+       <h3 className='votes'>({props.votes} votes)</h3>
+    </div>
+    :
+    <div className='rating'>
+        <img src="/public/Star.svg" alt="star" /><h3 className='rating'> No ratings</h3> 
+    </div>}
+    {!props.available &&  <h3 className='sold-out'>Sold out</h3>}
+    
+
 </div>
 )
 }
